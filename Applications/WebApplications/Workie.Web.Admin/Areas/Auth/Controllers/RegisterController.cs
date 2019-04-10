3abe61fd66985179e1d7a5ff78d17de8d47c5cfa
Shereen Akoum <shereenhakoum@gmail.com>
2019-04-10 15:23:46 +0300

    partial view of regsiter

--- Applications/WebApplications/Workie.Web.Admin/Areas/Auth/Controllers/RegisterController.cs
+++ Applications/WebApplications/Workie.Web.Admin/Areas/Auth/Controllers/RegisterController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace Workie.Web.Admin.Areas.Auth.Controllers
 {
@@ -8,6 +9,32 @@ namespace Workie.Web.Admin.Areas.Auth.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult VerifyEmail()
+        {
+            try
+            {     
+                return PartialView("Controls/_Controls_VerifyEmail");
+            }
+            catch (Exception ex)
+            {
+                // TODO: Will better handle this error, but for now this is fine.
+                return Content("An error was encountered. Please handle me!");
+            }
+        }
+
+        public IActionResult ViewRegister()
+        {
+            try
+            {
+                return PartialView("Controls/_Controls_RegisterView");
+            }
+            catch (Exception ex)
+            {
+                // TODO: Will better handle this error, but for now this is fine.
+                return Content("An error was encountered. Please handle me!");
+            }
         }
     }
 }
---
+++ Applications/WebApplications/Workie.Web.Admin/wwwroot/app/areas/auth/views/register/index.js
@@ -0,0 +1,71 @@
+﻿var RegisterResultModule = (function () {
+
+    function init() {
+        viewRegister();
+    }
+
+    function verifyEmail() {
+        $('#VerifyEmailTableForm').submit();
+    }
+
+    function viewRegister() {
+        $('#ViewRegisterTableForm').submit();
+    }
+
+    ////////////////////////////////////
+    // VIEW CALLBACKS
+    ////////////////////////////////////
+
+    function onViewRegisterSubmitBegin() {
+        //App.startPageLoading();
+        console.log('Submit began...');
+    }
+
+    function onViewRegisterSubmitSuccess(data) {
+        //App.stopPageLoading();
+        console.log('Submit Success!');
+    }
+
+    function onViewRegisterSubmitFailure(ajaxContext) {
+        //App.stopPageLoading();
+        console.log('Submit Failure, server error!');
+    }
+
+    ////////////////////////////////////
+    // Verify CALLBACKS
+    ////////////////////////////////////
+
+    function onVerifyEmailSubmitBegin() {
+        //App.startPageLoading();
+        console.log('Submit began...');
+    }
+
+    function onVerifyEmailSubmitSuccess(data) {
+        //App.stopPageLoading();
+        console.log('Submit Success!');
+    }
+
+    function onVerifyEmailSubmitFailure(ajaxContext) {
+        //App.stopPageLoading();
+        console.log('Submit Failure, server error!');
+    }
+
+   
+
+    // Reveal public pointers to private functions and properties
+    return {
+        init: init,
+        verifyEmail: verifyEmail,
+        onViewRegisterSubmitBegin: onViewRegisterSubmitBegin,
+        onViewRegisterSubmitSuccess: onViewRegisterSubmitSuccess,
+        onViewRegisterSubmitFailure: onViewRegisterSubmitFailure,
+        onVerifyEmailSubmitBegin: onVerifyEmailSubmitBegin,
+        onVerifyEmailSubmitSuccess: onVerifyEmailSubmitSuccess,
+        onVerifyEmailSubmitFailure: onVerifyEmailSubmitFailure,
+    };
+
+})();
+
+$(function () {
+    RegisterResultModule.init();
+});
