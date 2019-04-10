b624c3970cb2f40279b795e64ed550d5516175a3
Ahmad Chatila <47364209+ahmadnchatila@users.noreply.github.com>
2019-04-10 21:42:00 +0300

    Fixed profile

---
+++ Applications/WebApplications/Workie.Web.Admin/Areas/Account/Models/Profile/EditProfileViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Workie.Web.Admin.Areas.Account.Models.Profile
+{
+    public class EditProfileViewModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailAddress { get; set; }
+    }
+}
