7287fe4efbeb892c13326acdb92cdc066af33bd0
Ahmad Chatila <47364209+ahmadnchatila@users.noreply.github.com>
2019-04-15 16:23:15 +0300

    Register DB interaction ready

--- Applications/WebApplications/Workie.Web.Admin/Areas/Auth/Controllers/RegisterController.cs
+++ Applications/WebApplications/Workie.Web.Admin/Areas/Auth/Controllers/RegisterController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
+using Workie.Core.BusinessLogic.Users;
+using Workie.Core.Entities.Users;
+using Workie.Web.Admin.Areas.Auth.Models.Register;
 
 namespace Workie.Web.Admin.Areas.Auth.Controllers
 {
@@ -11,10 +14,47 @@ namespace Workie.Web.Admin.Areas.Auth.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel registerViewModel)
+        {
+            try
+            {
+                //TODO: Fix wrong values.
+                var userEntity = new UserEntity
+                {
+                    FirstName = registerViewModel.FirstName,
+                    LastName = registerViewModel.LastName,
+                    EmailAddress = registerViewModel.EmailAddress,
+                    _companyId = 1,
+                    _countryId = 1,
+                    _languageId = 1,
+                    _termsAndConditionId = "12345"
+                };
+
+                var userId = new UserManager().Insert(userEntity);
+
+                if (string.IsNullOrEmpty(userId))
+                {
+                    return Json(new
+                    {
+                        result = false,
+                        title = "Failure",
+                        message = "Failed to add a new user."
+                    });
+                }
+
+                return PartialView("UserControls/_UserControls_VerifyEmail");
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
         public IActionResult VerifyEmail()
         {
             try
-            {     
+            {
                 return PartialView("UserControls/_UserControls_VerifyEmail");
             }
             catch (Exception ex)
--- Applications/WebApplications/Workie.Web.Admin/Areas/Auth/Models/Register/RegisterViewModel.cs
+++ Applications/WebApplications/Workie.Web.Admin/Areas/Auth/Models/Register/RegisterViewModel.cs
@@ -2,6 +2,10 @@
 {
     public class RegisterViewModel
     {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
         public string EmailAddress { get; set; }
     }
 }
