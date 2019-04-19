c69a3f182d0d048fb0b238dd0c26b5ab2692563a
ShereenAkoum <48185092+ShereenAkoum@users.noreply.github.com>
2019-04-19 23:05:44 +0300

    Tasks UI + Quickbar Team added

---
+++ Applications/WebApplications/Workie.Web.Admin/Areas/Apps/Controllers/TasksController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Workie.Web.Admin.Filters;
+
+namespace Workie.Web.Admin.Areas.Apps.Controllers
+{
+    [Area("Apps")]
+    [Authorize]
+    [AuthorizeCurrentUser]
+    public class TasksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
