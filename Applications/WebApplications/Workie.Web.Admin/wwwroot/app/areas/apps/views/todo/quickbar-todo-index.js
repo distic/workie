9fffcece459b4237cb538dbeae0f76765ea588f9
Ahmad Shatila <ahmadnshatila@hotmail.com>
2019-04-24 00:55:28 +0300

    Finalized Todo functionality with UI

--- Applications/WebApplications/Workie.Web.Admin/wwwroot/app/areas/apps/views/todo/quickbar-todo-index.js
+++ Applications/WebApplications/Workie.Web.Admin/wwwroot/app/areas/apps/views/todo/quickbar-todo-index.js
@@ -2,10 +2,12 @@
 
     function init() {
         $('#TodoTableForm').submit();
+        handleDeleteTodo();
     }
 
     /* Add Task Block (QuickBar) */
     function addTaskBlockQB(title) {
+        $('#dt-table-empty-part').remove();
         $(".ms-quickbar-container .ms-todo-list").prepend(
             '<div class="ms-card ms-qa-card ms-deletable"> <div class="ms-card-header clearfix">' +
             '<h6 class="ms-card-title">' + title + '</h6> <button data-toggle="tooltip" data-placement="left" title="Add a Task to this block" class="ms-add-task-to-block ms-btn-icon float-right">' +
@@ -14,20 +16,21 @@
             '<i class="flaticon-trash"> </i> Delete </a> </div> </div>');
     }
 
-    function deleteTodoById(id) {
+    function handleDeleteTodo(id) {
+        $('body').on('click', '.ms-delete-trigger', function () {
 
-        $.ajax({
-            url: window.getDeleteTodoByIdUrl(id),
-            success: function (result) {
-                $(this).remove();
-            }
+            var handle = $(this);
+            var id = $(this).attr('value');
+
+            $.ajax({
+                url: window.getDeleteTodoByIdUrl(id),
+                success: function (result) {
+                    handle.closest('.ms-deletable').slideUp('slow', function () {
+                        handle.closest('.ms-deletable').remove();
+                    });
+                }
+            });
         });
-
-        //$('body').on('click', '.ms-delete-trigger', function () {
-        //    $(this).closest('.ms-deletable').slideUp('slow', function () {
-        //        $(this).closest('.ms-deletable').remove();
-        //    });
-        //});
     }
 
     function onRefreshTodoTableSubmitBegin() {
@@ -43,7 +46,15 @@
     }
 
     function onAddOrModifyTodoViewSubmitBegin() {
-        addTaskBlockQB($("#task-block").val());
+        var taskBlock = $("#task-block");
+
+        if (taskBlock.val() == '') {
+            alert('Empty fields are not allowed.');
+            return;
+        }
+
+        addTaskBlockQB(taskBlock.val());
+        taskBlock.val('');
     }
 
     function onAddOrModifyTodoViewSubmitSuccess(data) {
@@ -57,7 +68,7 @@
     return {
         init: init,
         addTaskBlockQB: addTaskBlockQB,
-        deleteTodoById: deleteTodoById,
+        handleDeleteTodo: handleDeleteTodo,
         onRefreshTodoTableSubmitBegin: onRefreshTodoTableSubmitBegin,
         onRefreshTodoTableSubmitSuccess: onRefreshTodoTableSubmitSuccess,
         onRefreshTodoTableSubmitFailure: onRefreshTodoTableSubmitFailure,
